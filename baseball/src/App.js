d97bea80600fd447969ccd064fba821e8c8cfd74
FreitagDavid <freitagdavid@outlook.com>
2019-01-30 10:26:47 -0500

    baseball/src/App: Partially done clickhandler

--- baseball/src/App.js
+++ baseball/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Display from './components/Display';
+import Dashboard from './components/Dashboard';
 import logo from './logo.svg';
 import './App.css';
 
@@ -10,7 +11,46 @@ class App extends Component {
             balls: 0,
             strikes: 0,
         };
+        this.shouldReset = this.state.balls === 4 || this.state.strikes === 3;
     }
+
+    clickHandler(type) {
+        switch (type) {
+            case 'hit':
+                this.setState({
+                    balls: 0,
+                    strikes: 0,
+                });
+                break;
+            case 'strike':
+                if (this.shouldReset) {
+                    this.setState({
+                        balls: 0,
+                        strikes: 0,
+                    });
+                    break;
+                }
+                this.setState({
+                    balls: this.state.balls + 1,
+                    strikes: this.state.strikes + 1,
+                });
+                break;
+            case 'foul':
+                if (this.strikes < 2) {
+                    this.setState({
+                        ...this.state,
+                        strikes: this.state.strikes + 1,
+                    });
+                }
+                break;
+            case 'ball':
+                if (this.state.balls < 3)
+            default:
+                break;
+        }
+    }
+
+    ball() {}
 
     render() {
         return (
@@ -19,6 +59,7 @@ class App extends Component {
                     balls={this.state.balls}
                     strikes={this.state.strikes}
                 />
+                <Dashboard />
             </div>
         );
     }
